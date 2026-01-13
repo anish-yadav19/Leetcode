41c1d8bad70cc31959164c02b6fd725411915720
Anish <65367707+anish-yadav19@users.noreply.github.com>
2026-01-13 13:20:53 +0530

    Time: 0 ms (100%), Space: 8.3 MB (14.35%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -2,29 +2,26 @@
     Company Tags                : GOOGLE
 */
 
-//Approach-1 (Simple Checks)
-//NOTE :  YOU can also use isupper() C++ function to check capital
+//Approach-2 (Small and simple)
 class Solution {
 public:
-    bool allCapitals(string word) {
-        for(char ch:word) {
-            if(ch <'A' || ch >'Z')
-                return false;
-        }
-        return true;
-    }
-    bool allSmall(string word) {
-        for(char ch:word) {
-            if(ch <'a' || ch >'z')
-                return false;
-        }
-        return true;
-    }
-    
     bool detectCapitalUse(string word) {
-        if(allCapitals(word) || allSmall(word) || allSmall(word.substr(1)))
+        int countCapitals = 0;
+        
+        for(char &ch : word) {
+            if(isupper(ch))
+                countCapitals++;
+        }
+        
+        if(countCapitals == 0)
             return true;
+        
+        if(countCapitals == word.length())
+            return true;
+        
+        if(countCapitals == 1 && isupper(word[0]))
+            return true;
+        
         return false;
     }
 };
-
