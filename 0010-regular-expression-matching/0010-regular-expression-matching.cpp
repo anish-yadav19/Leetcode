a5958fa7e0d8cd5cc71b5ace13e75876b51be1e5
Anish <65367707+anish-yadav19@users.noreply.github.com>
2026-01-22 12:29:28 +0530

    Time: 2690 ms (5.01%), Space: 16 MB (7.69%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,31 @@
+//Company Tags                : GOOGLE
+   
+
+
+//Approach-1 (Using Recursion - TLE for C++)
+//T.C : In worst case, the function may recursively call itself twice for each character in the pattern (due to *), leading to an exponential number of recursive calls.
+        //Hence, time complexity will be approaximately O(2^(max(n, m))). where n , m are lengths of s and p respectively.
+//S.C : O(m) - Recursion stack space : where m = length of pattern (maximum depth of recursion tree)
+        //NOTE : We are passing substr to isMatch every time and hence they are being copied to x and p so, we will be creating O(2^(max(n, m))) number of substrings.
+class Solution {
+public:
+    
+    bool isMatch(string text, string pattern) {
+        if (pattern.length() == 0) {
+            return text.length() == 0;
+        }
+
+        bool first_char_matched = false;
+        if(text.length() > 0 && (pattern[0] == text[0] || pattern[0] == '.')) {
+            first_char_matched = true;
+        }
+        
+        //Best example to understand : s = "aaab", p = "a*b"
+        if (pattern.length() >= 2 && pattern[1] == '*') {
+            return (isMatch(text, pattern.substr(2)) ||
+                    (first_char_matched && isMatch(text.substr(1), pattern)));
+        } else {
+            return first_char_matched && isMatch(text.substr(1), pattern.substr(1));
+        }
+    }
+};
