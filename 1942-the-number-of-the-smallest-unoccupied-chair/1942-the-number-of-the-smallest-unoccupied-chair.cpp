92290c45f4e6046056d6e7139db3ffc151332e87
Anish <65367707+anish-yadav19@users.noreply.github.com>
2026-01-29 20:38:51 +0530

    Time: 22 ms (87.8%), Space: 55 MB (99.61%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,40 +1,53 @@
 //Company Tags  		    : Uber, Facebook, Microsoft, Yelp, Google, Snapchat, Amazon, Cisco - Qn had small Variations
  
-//Approach-1 (Naive O(n^2) approach that comes to mind first)
-//T.C : O(n^2)
+//Approach-2 (Using min-heaps)
+//T.C : O(nlogn)
 //S.C : O(n)
 class Solution {
 public:
+    typedef pair<int, int> P;
+
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        
         int n = times.size();
+        priority_queue<P, vector<P>, greater<P> > occupied; //{departTime, chairNo}
+        priority_queue<int, vector<int>, greater<int>> free; //min heap of unoccupied chairs
         
-        vector<int> endTimes(n, -1); //at max we will have 0 to n-1 chairs
+        int targetFriendArrival = times[targetFriend][0];
+		
+        //Sort based on arrival time
+        sort(times.begin(),times.end());
         
-        /*
-            We need to sort the times based on arrival time but we don't want to
-            loose the friend number after sorting. So, store the arrival time
-            of targetFriend because it's given in the question that 
-            Each arrival time is distinct.
-        */
-        
-        int targetArrivalTime = times[targetFriend][0];
-        
-        sort(begin(times), end(times));
-        
-        for(vector<int> &time : times) {
-            int arrival = time[0];
-            int depart  = time[1];
-            //Find the first unoccupied chair
-            for(int i = 0; i<n; i++) {
-                if(endTimes[i] <= arrival) {
-                    endTimes[i] = depart; //update with current friend depart time
-                    
-                    if(arrival == targetArrivalTime)
-                        return i;
-                    break;
+		int chairNo = 0;
+
+        for(int i = 0; i < n; i++) {
+            int arrival  = times[i][0];
+            int depart   = times[i][1];
+            
+            //free chairs accordingly
+            while(!occupied.empty() && occupied.top().first <= arrival) {
+                free.push(occupied.top().second); //this chair is now free
+                occupied.pop();
+            }
+
+            if(free.empty()) {
+                occupied.push({depart, chairNo});
+
+                if(arrival == targetFriendArrival)
+                    return chairNo;
+
+                chairNo++;
+            } else {
+                int leastChairAvailable = free.top();
+                free.pop();
+                if(arrival == targetFriendArrival) {
+                    return leastChairAvailable;
                 }
+                occupied.push({depart, leastChairAvailable});
             }
         }
+
         return -1;
     }
+    
 };
