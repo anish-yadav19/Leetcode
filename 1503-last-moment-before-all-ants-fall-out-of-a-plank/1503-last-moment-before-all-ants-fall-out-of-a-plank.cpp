c592d467e991b55827de6778cd20a741f680313f
Anish <65367707+anish-yadav19@users.noreply.github.com>
2026-01-29 20:33:55 +0530

    Time: 0 ms (100%), Space: 27 MB (19.72%) - LeetHub

---
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,18 @@
+//T.C : O(m+n)
+//S.C : O(1)
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int result = 0;
+        
+        for(int &x : left) {
+            result = max(x, result);
+        }
+        
+        for(int &x : right) {
+            result = max(result, n-x);
+        }
+        
+        return result;
+    }
+};
